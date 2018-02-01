40acba535b18395e7e669927bd99d031c15eb6b3
geni429 <geni429@gmail.com>
2018-02-01 21:43:07 +0900

    HeaderNav css 적용

--- src/components/HeaderNav/HeaderNav.js
+++ src/components/HeaderNav/HeaderNav.js
@@ -4,11 +4,11 @@ import styles from './HeaderNav.css';
 const HeaderNav = () => {
   return (
     <header id={styles.header_nav}>
-      <div>
-        <span>Foodle</span>
+      <div className={styles.header_div}>
+        <span className={styles.header_content}>Foodle</span>
       </div>
-      <div>
-        <span>로그인/회원가입</span>
+      <div className={styles.header_div}>
+        <span className={styles.header_content}>로그인 / 회원가입</span>
       </div>
     </header>
   );
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -7,7 +7,6 @@ export default class Main extends Component {
     return (
       <div>
         {HeaderNav()}
-        <div>hello</div>
       </div>
     );
   }
