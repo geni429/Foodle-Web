4ac27e6fa106f15b6119505f7019f42585540086
geni429 <geni429@gmail.com>
2018-02-01 14:58:51 +0900

    Initialize project

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Main } from './containers';
+
+const rootElement = document.getElementById('root');
+render(<Main />, rootElement);
